bf04a9047fa64bd70a5ff302d707514a2c7f811b
Samuelk0nrad <samuel.ko702@gmail.com>
2025-05-11 14:34:50 +0200

    add error hanling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
-mcp = FastMCP("Ask MCP", "1.0.0")
+mcp = FastMCP("Ask MCP2", "0.2.1")
 temp_file = os.path.join(tempfile.gettempdir(), "ask_message_res_user.txt")
 
 def read_message_from_file(filename):
@@ -18,11 +18,11 @@ def read_message_from_file(filename):
     return message
 
 # Add an addition tool
-@mcp.tool("Ask-the-user", "Ask the user a question and get a response.")
+@mcp.tool("Ask", "Ask the user a question and get a response.")
 def ask(question: str, predictedAnswer: str | None = None) -> str:
     """Ask a question and get a response from the user."""
     subprocess.Popen([
-        "cmd", "/c", "start", "", sys.executable, "C:\\Users\\samue\\Documents\\dev\\python\\AskMCPServer\\ask-terminal.py", temp_file, question, predictedAnswer
+        "cmd", "/c", "start", "", sys.executable, "C:\\Users\\samue\\Documents\\dev\\python\\AskMCPServer\\ask-terminal.py", temp_file, question, predictedAnswer if predictedAnswer is not None else "No answer provided."
     ])
     print(f"Parent: Waiting for message from child... {temp_file}")
     message = read_message_from_file(temp_file)
