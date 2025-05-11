98c2a862ea74ebbe899328188bd09b40f80664d6
Samuelk0nrad <samuel.ko702@gmail.com>
2025-05-11 13:31:05 +0200

    script

---
+++ ask-terminal.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("No filename provided.")
+    sys.exit(1)
+
+filename = sys.argv[1].strip('"')
+question = sys.argv[2]
+
+user_input = input(f"{question} (Press Enter to finish): ")
