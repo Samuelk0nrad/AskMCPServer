01923d15d3a82fbbab17cfeb8ce9541350c669bb
Samuelk0nrad <samuel.ko702@gmail.com>
2025-05-11 12:30:23 +0200

    add test templade

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from askmcpserver!")
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Ask MCP", "1.0.0")
 
 
-if __name__ == "__main__":
-    main()
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
